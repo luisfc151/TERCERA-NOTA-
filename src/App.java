00656f1daecb59f828e7daa5ed08ea7dbb288ba6
luis <cuadroluisfelipe20@gmail.com>
2025-08-21 20:57:19 -0500

    primera semana

--- src/App.java
+++ src/App.java
@@ -1,5 +1,33 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    
+    public static class BancoApp {
+        // atributos 
+        String accountNumber;
+        String owner;
+        double balance;
+        
+        // metodos
+        void deposit(double amount){
+            balance += amount;
+            System.out.println("deposito" + amount + "nuevo balance: " + balance);
+        }
     }
-}
+     
+        public static void main(String[] args) throws Exception {
+            BancoApp account1 = new BancoApp(); 
+            account1. accountNumber = "555-555-555";
+            account1. balance = 2000000;
+            account1. owner = "luis";
+            account1.deposit(5000000);
+             
+            System.out.println(account1.balance);
+
+           
+            System.out.println(account1.owner);
+                      
+        }
+     
+        
+       }
+    
